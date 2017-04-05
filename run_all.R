48bdca3360d7d6fe724aa62a0c9e4497034e5251
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-04-05 02:59:54 -0500

    wrapper script

---
+++ run_all.R
@@ -0,0 +1,7 @@
+# JHU's Exploratory Data Analysis course at Coursera
+# Course Project 1: Wrapper Script
+
+## Run this script to produce the Course Project 1 plots
+source('~/github/ExData_Plotting1/0-get_clean_data.R') # get and clean data
+source('~/github/ExData_Plotting1/plot1.R') # plot 1
+source('~/github/ExData_Plotting1/plot2.R') # plot 2
