e3dc926153b4ab5a8b2709a2920f414a49b3f991
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-04-05 02:58:59 -0500

    getting and cleaning data

---
+++ 0-get_clean_data.R
@@ -0,0 +1,60 @@
+# JHU's Exploratory Data Analysis course at Coursera
+# Course Project 1
+# 0 - Reading the data
+
+## Loading required packages
+loader <- function(...) {
+    x <- list(...)
+    p <- x[sapply(X = x, FUN = is.character)]
+    for (i in 1:length(p)) {
+        if (!p[[i]] %in% installed.packages()) {
+            install.packages(p[[i]])
+        }
+        if (p[[i]] %in% installed.packages()) {
+            library(p[[i]], character.only = TRUE)
+        }
+    }
+}
+
+loader('dplyr', 'data.table')
+
+## Download zip file from URL from the Coursera assignment
+## Run only once
+# data.url <-
+#     "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#
+# download.file(url = data.url,
+#               destfile = "~/github/ExData_Plotting1/data.zip",
+#               method = "curl")
+#
+# unzip(zipfile = "data.zip") # unzip
+# file.remove("data.zip") # save space in repo
+
+## Loading the data from the *.txt file
+
+txt.files <- list.files(pattern = "*.txt")
+data.file <- ifelse(length(txt.files) == 1, txt.files, NULL)
+
+data <-
+    fread(
+        input = data.file,
+        sep = ";",
+        header = TRUE,
+        data.table = TRUE
+    )
+
+data <- subset(x = data, subset = Date %in% c('1/2/2007', '2/2/2017'))
+
+datetime <- paste(data$Date, data$Time)
+datetime <- strptime(x = datetime, format = "%d/%m/%Y %H:%M:%S") %>%
+    as.POSIXct()
+
+data$Date <- NULL
+data$Time <- NULL
+
+data <-
+    data %>% mutate_if(is.character, as.numeric)
+
+data <- cbind(datetime, data)
+
+rm(data.file, txt.files, loader, sub.data, datetime)
